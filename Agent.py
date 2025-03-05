bfcdea600a3f76ac3216af0a20158b098bdd59b6
Thad <144279670+ThadS97@users.noreply.github.com>
2025-03-05 11:44:04 -0800

    Update Agent.py
    Changed the top paragraph from line comments to a block comment

--- Agent.py
+++ Agent.py
@@ -1,11 +1,11 @@
-# The Agent class is a rudimentary artifical intelligence agent
-# that has four attributes: money (int), boughtReservation (Boolean),
-# isTodayTheWeekend (Boolean), and knowledgeBase (list). The knowledgeBase
-# list is a general list used for the agent to 'remember' its previous actions.
-# The __init__ function initializes the agent in the program while the addKnowledge
-# function adds knowledge of the agent's environment to the agent's 'memory.'
-# The knowledgeBase is currently not used in the program; will be incorporated at a
-# later date.
+""" The Agent class is a rudimentary artifical intelligence agent
+that has four attributes: money (int), boughtReservation (Boolean),
+isTodayTheWeekend (Boolean), and knowledgeBase (list). The knowledgeBase
+list is a general list used for the agent to 'remember' its previous actions.
+The __init__ function initializes the agent in the program while the addKnowledge
+function adds knowledge of the agent's environment to the agent's 'memory.'
+The knowledgeBase is currently not used in the program; will be incorporated at a
+later date. """
 
 class Agent:
     money = 100
